2c1683ee17c01337f23b4ccb0fb1b7ae2e43fb16
hieuluuchi <hieu.luu@opendigital.com.au>
2014-11-20 14:11:35 +0700

    display desktop version of term n conditoin

--- fanpromo/terms_n_conditions.php
+++ fanpromo/terms_n_conditions.php
@@ -11,6 +11,8 @@ include_once('../languages/'.LANGCODE.'/soc.php');
 include_once('../languages/'.LANGCODE.'/foodwine/index.php');
 
 $smarty->assign('terms_page', tab_content($dbcon, 5));
+$smarty->assign('page_group', "fanpromo");
+$smarty->assign('hide_responsive', true);
 display_page($dbcon, $smarty, 'terms_n_conditions.tpl', 'Terms and Conditions', $_LANG);
 unset($smarty);
 ?>
